4290d034699fe8d7abeeb3bb3b5b61186e33b5df
LeeJin0527 <ijin17463@gmail.com>
2022-05-10 17:51:28 +0900

    catchCallUrl 적용
    다른 페이지는 필요 없어서  모든 url 은main Page로 이동

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const app = express();
 app.set('view engine', 'pug');
 app.set("views", __dirname +"/views");
 app.get("/", (req, res)=> res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 app.use("/public", express.static(__dirname + "/public"));
 console.log("hello");
 app.listen(3000);
