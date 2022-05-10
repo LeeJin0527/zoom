3ad61d189fb5b352e78ddb7115a99caed593a921
LeeJin0527 <ijin17463@gmail.com>
2022-05-10 17:37:41 +0900

    Frontend Setup

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 const app = express();
-
+app.set('view engine', 'pug');
+app.set("views", __dirname +"/views");
+app.get("/", (req, res)=> res.render("home"));
+app.use("/public", express.static(__dirname + "/public"));
 console.log("hello");
 app.listen(3000);
