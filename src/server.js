3ab0f75c970783954df4dc1116fde561979d49cd
LeeJin0527 <ijin17463@gmail.com>
2022-05-11 16:01:51 +0900

    WebSocket 백엔드 프론트엔트 연결

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 import express from "express";
+import WebSocket , {WebSocketServer} from "ws";
+import http from "http";
+
+
 const app = express();
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 app.set("views", __dirname +"/views");
-app.get("/", (req, res)=> res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
 app.use("/public", express.static(__dirname + "/public"));
-console.log("hello");
-app.listen(3000);
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+function handleConnection(socket) {
+    console.log(socket);
+}
+
+wss.on("connection" , handleConnection); // on method : 이벤트를 기다림
+
+server.listen(3000, handleListen); // http 서버 위에 ws 서버 만들고 내 http 서버에 access
